787eb09c52fbb2bbd67b83432502113d9090251c
Julien Afonso <ju.afonso61@gmail.com>
2023-06-21 23:03:29 +0900

    crudino

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -1,9 +1,42 @@
 class DinosaursController < ApplicationController
+  before_action :set_dinosaur, only: [:show, :edit, :update, :destroy]
   def index
     @dinosaurs = Dinosaur.all
   end
 
   def show
-    @dinosaur = Dinosaur.find(params[:id])
   end
+
+  def new
+    @dinosaur = Dinosaur.new
+  end
+
+  def create
+    @dinosaur = Dinosaur.new(dinosaur_params)
+    @dinosaur.save
+    redirect_to dinosaur_path(@dinosaur)
+  end
+
+  def edit
+  end
+
+  def update
+    @dinosaur.update(dinosaur_params)
+    redirect_to dinosaur_path(@dinosaur)
+  end
+
+  def destroy
+    @dinosaur.destroy
+    redirect_to dinosaurs_path, status: :see_other
+  end
+end
+
+private
+
+def set_dinosaur
+  @dinosaur = Dinosaur.find(params[:id])
+end
+
+def dinosaur_params
+  params.require(:dinosaur).permit(:name, :group, :japanese)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get 'dinosaurs', to: 'dinosaurs#index'
-  get 'dinosaurs/:id', to: 'dinosaurs#show'
+  get 'dinosaurs/new', to: 'dinosaurs#new'
+  get 'dinosaurs/:id', to: 'dinosaurs#show', as: :dinosaur
+  post 'dinosaurs', to: 'dinosaurs#create'
+  get 'dinosaurs/:id/edit', to: 'dinosaurs#edit'
+  patch 'dinosaurs/:id', to: 'dinosaurs#update'
+  delete 'dinosaurs/:id', to: 'dinosaurs#destroy'
 end
