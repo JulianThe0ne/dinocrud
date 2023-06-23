ca8b2b2c92f5fa1bd9a04912d9f177e7c293c4a2
Julien Afonso <ju.afonso61@gmail.com>
2023-06-23 16:50:34 +0900

    seed 10 dinosaurs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'dinosaurs', to: 'dinosaurs#index'
-  get 'dinosaurs/new', to: 'dinosaurs#new'
-  get 'dinosaurs/:id', to: 'dinosaurs#show', as: :dinosaur
-  post 'dinosaurs', to: 'dinosaurs#create'
-  get 'dinosaurs/:id/edit', to: 'dinosaurs#edit'
-  patch 'dinosaurs/:id', to: 'dinosaurs#update'
-  delete 'dinosaurs/:id', to: 'dinosaurs#destroy'
+  resources :dinosaurs
+  # get 'dinosaurs', to: 'dinosaurs#index'
+  # get 'dinosaurs/new', to: 'dinosaurs#new'
+  # get 'dinosaurs/:id', to: 'dinosaurs#show', as: :dinosaur
+  # post 'dinosaurs', to: 'dinosaurs#create'
+  # get 'dinosaurs/:id/edit', to: 'dinosaurs#edit'
+  # patch 'dinosaurs/:id', to: 'dinosaurs#update'
+  # delete 'dinosaurs/:id', to: 'dinosaurs#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning the database'
+Dinosaur.destroy_all
+
+puts 'Creating Dinosaurs...'
+
+Dinosaur.create(name: 'tyrannosaurus', group: 'carnivorous', japanese: 'ティラノサウルス')
+Dinosaur.create(name: 'triceratops', group: 'herbivorous', japanese: 'トリケラトプス')
+Dinosaur.create(name: 'spinosaurus', group: 'carnivorous', japanese: 'スピノサウルス')
+Dinosaur.create(name: 'stegosaurus', group: 'herbivorous', japanese: 'ステゴサウルス')
+Dinosaur.create(name: 'allosaurus', group: 'carnivorous', japanese: 'アロサウルス')
+Dinosaur.create(name: 'Apatosaurus', group: 'herbivorous', japanese: 'アパトサウルス')
+Dinosaur.create(name: 'brachiosaurus', group: 'herbivorous', japanese: 'ブラキオサウルス')
+Dinosaur.create(name: 'tyrannosaurus rex', group: 'carnivorous', japanese: 'ティラノサウルス・レックス')
+Dinosaur.create(name: 'velociraptor', group: 'carnivorous', japanese: 'ヴェロキラプトル')
+Dinosaur.create(name: 'ankylosaurus', group: 'herbivorous', japanese: 'アンキロサウルス')
+
+puts 'Finished'
